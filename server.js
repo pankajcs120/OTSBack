12b991c4fece33d1787728d8ce11f66e69b05cf2
pankajcs120 <128893521+pankajcs120@users.noreply.github.com>
2024-02-21 15:18:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGODB_URI,{
 .catch((error)=>console.log(error));
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "ots-front-eta.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
